fd4c140221621abc599d41fa8748f0d45e4748ec
Maurício Citadini Biléssimo <mauricioc.bilessimo@gmail.com>
2024-03-21 15:25:36 -0300

    Commit inicial.

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+        <link rel="stylesheet" href="public/css/style.css">
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>Materialize</title>
+    </head>
+    <body>
+        <div id="sidebar" class="z-depth-2">
+            <div id="sidebar-header">
+                <a href="#">
+                    <img src="public/images/logo.png" alt="Logo">
+                </a>
+            </div>
+            <ul class="sidebar-menu">
+                <li class="item-menu">
+                    <a href="#" class="link-menu">Dashboard</a>
+                </li>
+                <li class="item-menu">
+                    <a href="#" class="link-menu">Agendamentos</a>
+                </li>
+                <li class="item-menu">
+                    <a href="#" class="link-menu">Clientes</a>
+                </li>
+                <li class="item-menu">
+                    <a href="#" class="link-menu">Serviços</a>
+                </li>
+            </ul>
+        </div>
+
+        <nav id="nav-header" class="white">
+            <div class="nav-wrapper">
+                <ul id="nav-mobile" class="right hide-on-med-and-down">
+                    <li><a href="sass.html" class="orange-text darken-2">Sass</a></li>
+                    <li><a href="badges.html" class="orange-text darken-2">Components</a></li>
+                    <li><a href="collapsible.html" class="orange-text darken-2">JavaScript</a></li>
+                </ul>
+            </div>
+        </nav>
+
+        <section id="main">
+            <h4>Bem vindo Maurício Citadini Biléssimo</h4>
+            <hr>
+        </section>
+
+        <!--JavaScript at end of body for optimized loading-->
+        <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
+    </body>
+</html>
